4a90011099b51984a80db8ac4907260e2493ff69
Rafael De Luca <deluca1712@gmail.com>
2021-01-27 14:15:09 -0300

    Project Created

---
+++ src/main/java/br/com/delucahigiene/curso/CursoWebserverApplication.java
@@ -0,0 +1,13 @@
+package br.com.delucahigiene.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoWebserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoWebserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/delucahigiene/curso/CursoWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.delucahigiene.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoWebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
